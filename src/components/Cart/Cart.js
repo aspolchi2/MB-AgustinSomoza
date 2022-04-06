676db836bb93963ef7526f3a4d2605b7865fe303
aspolchi <aspolchi@gmail.com>
2022-04-06 11:12:40 -0300

    i little bit of style

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useContext } from "react";
-import { Card, ListGroup, ListGroupItem } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
 import Button from "../../helpers/Button/Button";
@@ -24,35 +23,28 @@ const Cart = () => {
   return (
     <div className="row cart">
       <h2>Resumen de tu carrito</h2>
+      {cart.map((item) => (
+        <article className="cartCard">
+          <header className="cardHeader">
+            <img src={item.img} className="asd"></img>
+          </header>
+          <main className="cardMain">
+            <h3>
+              {item.name} {item.count === 1 || `X${item.count}`}
+            </h3>
+            <Price amount={item.price * item.count} />
+            <Button text={"Eliminar"} onClick={() => removeItem(item.id)} />
+          </main>
+        </article>
+      ))}
       <div>
-        <div>
-          <p className="textAlign">Total</p>{" "}
-          <Price amount={cartTotal()} className={"textAlign"} />
-        </div>
+        <Price text={'Total:'} amount={cartTotal()} className={"textAlign"} />
         <Button
           text={"Vaciar Carrito"}
           onClick={emptyCart}
           fixed={"textAlign"}
         />
       </div>
-      {cart.map((item, id) => (
-        <Card style={{ width: "20rem" }} key={item.id}>
-          <Card.Img variant="top" src={item.img} />
-          <Card.Body>
-            <Card.Title>{item.name}</Card.Title>
-            <Card.Text>{item.desc}</Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroupItem>Cantidad: {item.count}</ListGroupItem>
-            <ListGroupItem>
-              <Price amount={item.price * item.count} />
-            </ListGroupItem>
-          </ListGroup>
-          <Card.Body>
-            <Button text={"Eliminar"} onClick={() => removeItem(item.id)} />
-          </Card.Body>
-        </Card>
-      ))}
     </div>
   );
 };
--- src/helpers/Price.js
+++ src/helpers/Price.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import formatCurrency from './formatCurrency';
 
-export const Price = ({amount, className}) => {
+export const Price = ({amount, className, text}) => {
 
     const formatAmount = formatCurrency(amount);
   return (
-    <p className={`detailPrice ${className}`}>{formatAmount}</p>
+    <p className={`detailPrice ${className}`}>{text} {formatAmount}</p>
   )
 }
