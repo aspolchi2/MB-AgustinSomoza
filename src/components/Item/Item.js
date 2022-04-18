b62bd25a213b88a881e31651f7a7b7c773a9194f
Agustin Somoza Polchi <aspolchi@gmail.com>
2022-04-18 15:47:42 -0300

    Update Item.js

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,24 +4,29 @@ import { Link } from "react-router-dom";
 import MyButton from "../Button/MyButton";
 import { Price } from "../../helpers/Price";
 
-export const Item = ({  id,  name,  price,  desc,  img,  stock,  initial,  category}) => {
+export const Item = ({
+  id,
+  name,
+  price,
+  desc,
+  img,
+  stock,
+  initial,
+  category,
+}) => {
   return (
     <Card style={{ width: "15rem" }}>
-      <Card.Img variant="top" src={img} className="img-fluid"/>
+      <Card.Img variant="top" src={img} className="img-fluid" />
       <Link to={`/detail/${id}`}>
-        <MyButton text={'Ver detalles'} />
+        <MyButton text={"Ver detalles"} />
       </Link>
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        {
-          stock !== 0 || <p className="lastStock">Sin stock disponible</p> 
-        }
-
-        
         <Card.Text>
           <Price amount={price} />
           <br></br>
-          <span>Stock disponible : {stock}</span>
+          <span>{stock === 0 ? 'Sin stock disponible' : `Stock displinble: ${stock}`}</span>
+           <p className="lastStock">{stock !== 1 || 'Ultimo en Stock'}</p>
         </Card.Text>
       </Card.Body>
     </Card>
