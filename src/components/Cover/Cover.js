6a6c1c9d370a7b802582559b4b0858a5c79e3e87
Agustin Somoza Polchi <aspolchi@gmail.com>
2022-04-25 22:30:43 -0300

    details

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { LinkContainer } from "react-router-bootstrap";
-import { coverData } from "../../data/coverData";
 import { useMobile } from "../../Hooks/useMobile";
 import headerImg from "./header.jpg";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../firebase/config";
 
 const Cover = () => {
   const { isMobile } = useMobile();
-  //early return if isMobile is true
+  const [cover, setCover] = useState([]);
+  
+  useEffect(() => {
+    const coverRef = collection(db, "cover");
+    getDocs(coverRef).then((data) => {
+      setCover(
+        data.docs.map((doc) => {
+          return { ...doc.data() };
+        })
+      );
+    });
+  }, [isMobile]);
+
   if (isMobile === true) {
     return (
       <div>
@@ -16,18 +29,21 @@ const Cover = () => {
           <p>Somos apasionadas</p>
         </div>
         <div className="coverMainMobile">
-        {coverData.map((res) => (
-          <div className={`coverItemMobile`}>
-            <LinkContainer to={`/category/${res.name}`}>
-              <img src={res.imgMobile} alt={res.name}></img>
-            </LinkContainer>
-          </div>
-        ))}
+          {
+            cover.map((data, i)=> (
+              <LinkContainer to={`/category/${data.name}`} key={i}>
+                <img src={data.imgMobile} alt={data.name}></img>
+              </LinkContainer>
+              
+              
+            ))
+          }
+          
+          
         </div>
       </div>
     );
   }
-  // normal return if is desktop
   return (
     <div className="cover">
       <div className="coverHeader">
@@ -43,7 +59,7 @@ const Cover = () => {
         </div>
       </div>
       <div className="coverMain">
-        {coverData.map((item, i) => (
+        {cover.map((item, i) => (
           <div key={i} className={`coverItems ${item.name}`}>
             <LinkContainer to={`/category/${item.name}`}>
               <img src={item.img} alt={item.name}></img>
